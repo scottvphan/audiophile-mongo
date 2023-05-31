48b044b2a2855ee8ec8459554404710315f3966d
scottvphan <scott19023@gmail.com>
2023-05-31 13:52:10 -0500

    Added global app styles and page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import HomePage from './pages/homePage'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
+import HomePage from './pages/HomePage'
+import Layout from './components/Layout'
+import { Route } from 'react-router-dom'
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  :root {
+    margin: 0;
+    padding: 0;
+    font-family:'Manrope', Arial, Helvetica, sans-serif;
+  }
+  button {
+    font-family:'Manrope', Arial, Helvetica, sans-serif;
+  }
+`
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/" element={<HomePage />}/>
+    </Route>
+  )
+)
 
 function App() {
 
   return (
-    <HomePage />
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
   )
 }
 
