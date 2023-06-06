01e84ddb644286776cb55da58c06e9dc3990f0ab
scottV-P <114790711+scottV-P@users.noreply.github.com>
2023-06-06 09:04:22 -0400

    added product category page to the routing layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 import HomePage from './pages/HomePage'
 import Layout from './components/Layout'
 import { Route } from 'react-router-dom'
+import ProductCategoryPage from './pages/ProductCategoryPage'
 
 const GlobalStyle = createGlobalStyle`
   html,
@@ -21,6 +22,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<HomePage />}/>
+      <Route path="/product" element={<ProductCategoryPage />}>
+        <Route path='/product:id' element={<ProductCategoryPage />} />
+      </Route>
     </Route>
   )
 )
