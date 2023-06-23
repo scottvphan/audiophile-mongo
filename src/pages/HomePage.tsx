7ed1e4339dc6457dda67caf58566c0942e2b0d82
scottvphan <scott19023@gmail.com>
2023-06-22 21:33:32 -0500

    changed home page css

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,82 +1,82 @@
-import mobileImage from '/assets/home/mobile/image-header.jpg';
-import tabletImage from '/assets/home/tablet/image-header.jpg';
-import desktopImage from '/assets/home/desktop/image-header.jpg';
-import styled from "styled-components"
-import ProductPreview from '../components/ProductPreview';
-import { useAuth0 } from '@auth0/auth0-react'
-import LogoutButton from '../components/LogoutButton';
-import LoginButton from '../components/LoginButton';
-import ProductCardList from '../components/ProductCardList';
+import mobileImage from "/assets/home/mobile/image-header.jpg";
+import tabletImage from "/assets/home/tablet/image-header.jpg";
+import desktopImage from "/assets/home/desktop/image-header.jpg";
+import styled from "styled-components";
+import ProductPreview from "../components/ProductPreview";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "../components/LogoutButton";
+import LoginButton from "../components/LoginButton";
+import ProductCardList from "../components/ProductCardList";
 
-export default function HomePage(){
-    const StyledDiv = styled.div`
-        height:100%;
-        background-color: white;
-    `
-    const NewProductContainer = styled.div`
-        color:white;
-        display:flex;
-    `
-    const StyledSection = styled.section`
-        padding:0 20rem;
-        width:100%;
-        background-image: url(${desktopImage});
-        background-position: center bottom;
-        background-repeat: no-repeat;
-        display:flex;
-        background-size:cover;
-        height:50rem;
-        @media screen and (max-width:1024px) {
-            background-image: url(${tabletImage});
-        }
-        @media screen and (max-width:768px) {
-            background-image: url(${mobileImage});
-        }
-    `
-    const StyledHeading = styled.h1`
-        font-style: normal;
-        font-weight: 700;
-        font-size: 56px;
-        line-height: 58px;
-        letter-spacing: 2px;
-    `
-    const ProductHeading = styled.h5`
-        font-family: 'Manrope';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 14px;
-        line-height: 19px;
-        letter-spacing: 10px;
-        color:#FFFFFF;
-        opacity:0.5;
-    `
-    const ProductDescription = styled.p`
-        font-style: normal;
-        font-weight: 500;
-        font-size: 15px;
-        line-height: 25px;
-        color: #FFFFFF;
-        mix-blend-mode: normal;
-        opacity: 0.75;
-    `
-    const ProductContainer = styled.div`
-        width:45%;
-        margin:auto 0;
-    `
-    const ProductButton = styled.button`
-        background-color: #D87D4A;
-        padding:1rem;
-        color:white;
-        font-style: normal;
-        font-weight: 700;
-        font-size: 13px;
-        line-height: 18px;
-        letter-spacing: 1px;
-    `
+const StyledDiv = styled.div`
+    height: 100%;
+    background-color: white;
+`;
+const NewProductContainer = styled.div`
+    color: white;
+    display: flex;
+`;
+const StyledSection = styled.section`
+    padding: 0 20rem;
+    width: 100%;
+    background-image: url(${desktopImage});
+    background-position: center bottom;
+    background-repeat: no-repeat;
+    display: flex;
+    background-size: cover;
+    height: 50rem;
+    @media screen and (max-width: 1024px) {
+        background-image: url(${tabletImage});
+    }
+    @media screen and (max-width: 768px) {
+        background-image: url(${mobileImage});
+    }
+`;
+const StyledHeading = styled.h1`
+    font-style: normal;
+    font-weight: 700;
+    font-size: 56px;
+    line-height: 58px;
+    letter-spacing: 2px;
+`;
+const ProductHeading = styled.h5`
+    font-family: "Manrope";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19px;
+    letter-spacing: 10px;
+    color: #ffffff;
+    opacity: 0.5;
+`;
+const ProductDescription = styled.p`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 25px;
+    color: #ffffff;
+    mix-blend-mode: normal;
+    opacity: 0.75;
+`;
+const ProductContainer = styled.div`
+    width: 45%;
+    margin: auto 0;
+`;
+const ProductButton = styled.button`
+    background-color: #d87d4a;
+    padding: 1rem;
+    color: white;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 13px;
+    line-height: 18px;
+    letter-spacing: 1px;
+`;
 
+export default function HomePage() {
     const { isAuthenticated } = useAuth0();
-    
-    return(
+
+    return (
         <>
             {isAuthenticated ? <LogoutButton /> : <LoginButton />}
             <StyledDiv>
@@ -84,8 +84,14 @@ export default function HomePage(){
                     <StyledSection>
                         <ProductContainer>
                             <ProductHeading>NEW PRODUCT</ProductHeading>
-                            <StyledHeading>XX99 MARK II HEADPHONES</StyledHeading>
-                            <ProductDescription>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast</ProductDescription>
+                            <StyledHeading>
+                                XX99 MARK II HEADPHONES
+                            </StyledHeading>
+                            <ProductDescription>
+                                Experience natural, lifelike audio and
+                                exceptional build quality made for the
+                                passionate music enthusiast
+                            </ProductDescription>
                             <ProductButton>SEE PRODUCT</ProductButton>
                         </ProductContainer>
                     </StyledSection>
@@ -94,5 +100,5 @@ export default function HomePage(){
                 <ProductPreview />
             </StyledDiv>
         </>
-    )
+    );
 }
