6a2746a42121e05a2d6961baff2d18d1db595478
scottvphan <scott19023@gmail.com>
2023-06-22 21:25:18 -0500

    moved some app code to the index to render the app differently depending on if the user is logged in or not

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,30 @@
-import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
-import HomePage from './pages/HomePage'
-import Layout from './components/Layout'
-import { Route } from 'react-router-dom'
-import ProductCategoryPage from './pages/ProductCategoryPage'
-import ProductDetailPage from './pages/ProductDetailPage'
-import { useState, useEffect } from 'react';
-import { Auth0Provider } from '@auth0/auth0-react';
-
-const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  :root {
-    margin: 0;
-    padding: 0;
-    font-family:'Manrope', Arial, Helvetica, sans-serif;
-  }
-  button {
-    font-family:'Manrope', Arial, Helvetica, sans-serif;
-    cursor:pointer;
-  }
-`
+import { useState, useEffect } from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
+import Index from "./Index";
 
 function App() {
-    const [data, setData] = useState<unknown>('')
-  useEffect(() =>{
-    fetch("/data.json")
-      .then(response => response.json())
-      .then(data => setData(data))
-  }, [])
-  
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductCategoryPage data={data} />}>
-          <Route path='/products/:id' element={<ProductCategoryPage data={data} />}/>
-        </Route>
-        <Route path="/products/details/:id" element={<ProductDetailPage data={data} />} />
-      </Route>
-    )
-  );  
+    const [data, setData] = useState<unknown>("");
+    useEffect(() => {
+        fetch("/data.json")
+            .then((response) => response.json())
+            .then((data) => setData(data));
+    }, []);
 
-  return (
-    <>
-      <Auth0Provider
-        domain="dev-g4y2r5dknwja6vmn.us.auth0.com"
-        clientId="lBcslrA0ORiR01tbzvT39N3mVItYqbsZ"
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-      >
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </Auth0Provider>
-    </>
-  )
+    return (
+        <>
+            <Auth0Provider
+                domain="dev-g4y2r5dknwja6vmn.us.auth0.com"
+                clientId="lBcslrA0ORiR01tbzvT39N3mVItYqbsZ"
+                authorizationParams={{
+                    redirect_uri: window.location.origin,
+                }}
+            >
+                <>
+                    <Index data={data} />
+                </>
+            </Auth0Provider>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Index.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import HomePage from "./pages/HomePage";
+import Layout from "./components/Layout";
+import { Route } from "react-router-dom";
+import ProductCategoryPage from "./pages/ProductCategoryPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body,
+    :root {
+        margin: 0;
+        padding: 0;
+        font-family:'Manrope', Arial, Helvetica, sans-serif;
+    }
+    button {
+        font-family:'Manrope', Arial, Helvetica, sans-serif;
+        cursor:pointer;
+    }
+    input{
+        font-family:'Manrope', Arial, Arial, Helvetica, sans-serif
+    }
+    SVG {
+        cursor: pointer;
+        path{
+            transition:0.3s;
+        }
+        &:hover{
+            path{
+                transition:0.3s;
+                fill:#D87D4A;
+            }
+        }
+        user-select: none;
+    }
+    img{
+        user-select: none;
+    }
+`;
+
+export default function Index({ data }: any) {
+    const { isLoading } = useAuth0();
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route
+                    path="/products"
+                    element={<ProductCategoryPage data={data} />}
+                >
+                    <Route
+                        path="/products/:id"
+                        element={<ProductCategoryPage data={data} />}
+                    />
+                </Route>
+                <Route
+                    path="/products/details/:id"
+                    element={<ProductDetailPage data={data} />}
+                />
+                <Route path="/checkout" element={<CheckoutPage />} />
+            </Route>
+        )
+    );
+    return (
+        <>
+            <GlobalStyle />
+            {isLoading ? <></> : <RouterProvider router={router} />}
+        </>
+    );
+}
