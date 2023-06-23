d589cedf28cd8c1dc41cd7278f6ec2fdfa36a207
scottvphan <scott19023@gmail.com>
2023-06-22 21:30:00 -0500

    added hover styling to the nav and footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Footer = styled.footer`
-    background-color:black;
+    background-color: black;
     /* background-color: #101010; */
     color: white;
 `;
@@ -20,26 +20,31 @@ const StyledLink = styled(Link)`
     color: white;
     font-weight: 700;
     text-decoration: none;
+    transition: 0.3s;
+    &:hover {
+        color: #d87d4a;
+        transition: 0.3s;
+    }
 `;
 const LeftContainer = styled.div`
-    width:50%;
+    width: 50%;
 `;
 const RightContainer = styled.div`
-    width:50%;
-    display:flex;
+    width: 50%;
+    display: flex;
     justify-content: flex-end;
-    gap:1rem;
+    gap: 1rem;
     align-items: center;
 `;
 const TopFooterContainer = styled.div`
-    display:flex;
+    display: flex;
     justify-content: space-between;
     align-items: center;
 `;
 const BottomFooterContainer = styled.div`
-    display:flex;
-    gap:20rem;
-`
+    display: flex;
+    gap: 20rem;
+`;
 const StyledParagraph = styled.p`
     font-style: normal;
     font-weight: 500;
@@ -47,7 +52,7 @@ const StyledParagraph = styled.p`
     line-height: 25px;
     mix-blend-mode: normal;
     opacity: 0.5;
-`
+`;
 export default function Navbar() {
     return (
         <Footer>
@@ -55,22 +60,30 @@ export default function Navbar() {
                 <TopFooterContainer>
                     <h1>audiophile</h1>
                     <LinkContainer>
-                    <StyledLink to="/">HOME</StyledLink>
-                    <StyledLink to="/products/headphones">HEADPHONES</StyledLink>
-                    <StyledLink to="/products/speakers">SPEAKERS</StyledLink>
-                    <StyledLink to="/products/earphones">EARPHONES</StyledLink>
+                        <StyledLink to="/">HOME</StyledLink>
+                        <StyledLink to="/products/headphones">
+                            HEADPHONES
+                        </StyledLink>
+                        <StyledLink to="/products/speakers">
+                            SPEAKERS
+                        </StyledLink>
+                        <StyledLink to="/products/earphones">
+                            EARPHONES
+                        </StyledLink>
                     </LinkContainer>
                 </TopFooterContainer>
                 <BottomFooterContainer>
                     <LeftContainer>
                         <StyledParagraph>
-                            Audiophile is an all in one stop to fulfill your audio
-                            needs. We're a small team of music lovers and sound
-                            specialists who are devoted to helping you get the most
-                            out of personal audio. Come and visit our demo facility
-                            - we’re open 7 days a week.
+                            Audiophile is an all in one stop to fulfill your
+                            audio needs. We're a small team of music lovers and
+                            sound specialists who are devoted to helping you get
+                            the most out of personal audio. Come and visit our
+                            demo facility - we’re open 7 days a week.
                         </StyledParagraph>
-                        <StyledParagraph>Copyright 2021. All Rights Reserved</StyledParagraph>
+                        <StyledParagraph>
+                            Copyright 2021. All Rights Reserved
+                        </StyledParagraph>
                     </LeftContainer>
                     <RightContainer>
                         <SVG src="/assets/facebook.svg"></SVG>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,51 +1,72 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import SVG from 'react-inlinesvg'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import SVG from "react-inlinesvg";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "/assets/shared/desktop/logo.svg";
 
 const Nav = styled.nav`
-    background-color:#101010;
+    background-color: #101010;
     /* background-color:black; */
-    color:white;
+    color: white;
     padding: 0rem 20rem;
-    z-index:1;
+    z-index: 1;
     position: relative;
-`
+`;
 const LinkContainer = styled.div`
-    display:flex;
-    gap:20px;
-`
+    display: flex;
+    gap: 20px;
+`;
 const NavContainer = styled.div`
-    display:flex;
+    display: flex;
     justify-content: space-between;
     align-items: center;
     border-bottom: 1px solid #ffffff33;
-    padding:0.5rem 0;
-`
+    padding: 0.5rem 0;
+`;
 const StyledLink = styled(Link)`
-    color:white;
-    font-weight:700;
-    text-decoration:none;
-`
+    color: white;
+    font-weight: 700;
+    text-decoration: none;
+    transition: 0.3s;
+    &:hover {
+        color: #d87d4a;
+        transition: 0.3s;
+    }
+`;
 const StyledSVG = styled(SVG)`
-    cursor:pointer;
-`
-export default function Navbar({setIsCartOpen, isCartOpen}:any){
-    console.log()
-    return(
+    cursor: pointer;
+    :first-of-type {
+        &:hover {
+            color: white !important;
+            cursor: default;
+            path {
+                transition: 0.3s;
+                fill: white;
+            }
+        }
+    }
+`;
+export default function Navbar({ setIsCartOpen, isCartOpen }: any) {
+    console.log();
+    return (
         <Nav>
             <NavContainer>
-                <h1>audiophile</h1>
+                <StyledSVG src={logo} />
                 <LinkContainer>
                     <StyledLink to="/">HOME</StyledLink>
-                    <StyledLink to="/products/headphones">HEADPHONES</StyledLink>
+                    <StyledLink to="/products/headphones">
+                        HEADPHONES
+                    </StyledLink>
                     <StyledLink to="/products/speakers">SPEAKERS</StyledLink>
                     <StyledLink to="/products/earphones">EARPHONES</StyledLink>
                 </LinkContainer>
-                <StyledSVG onClick = {() =>{
-                    setIsCartOpen(!isCartOpen)
-                }} src='/assets/shared/desktop/icon-cart.svg'/>
+                <StyledSVG
+                    onClick={() => {
+                        setIsCartOpen(!isCartOpen);
+                    }}
+                    src="/assets/shared/desktop/icon-cart.svg"
+                />
             </NavContainer>
         </Nav>
-    )
+    );
 }
